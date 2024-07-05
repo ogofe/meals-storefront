a6665b1a8c407cc3772492c2112305cf0637e3cf
Ogofe <7thogofe@gmail.com>
2024-07-05 19:31:18 +0100

    add default store

--- src/helpers/hooks.js
+++ src/helpers/hooks.js
@@ -2,7 +2,8 @@
 
 function useSubdomain(){
 	try{
-		let sub = window.location.hostname.split('.')[0] || 'the-ring'
+		// let sub = window.location.hostname.split('.')[0] || 'the-ring'
+		let sub = 'the-ring'
 		return sub
 	}catch(err){
 		throw new Error("useSubdomain must be called within a page context")
